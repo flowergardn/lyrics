3d078db8cb1ee3879849e3a54fd54aaaee3ca2f7
astridlol <sites@astrid.sh>
2023-09-09 01:27:49 -0400

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,14 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 
 export default config;
